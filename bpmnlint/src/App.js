32461ca3d5a69c324329bffb65e8b1f05d979300
imdwpeng <imdwpeng@foxmail.com>
2018-12-27 15:10:54 +0800

    bpmnlint

--- bpmnlint/src/App.js
+++ bpmnlint/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import EditorBasic from './containers';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div style={{ height: '100%', overflow: 'hidden' }}>
-        <EditorBasic />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
