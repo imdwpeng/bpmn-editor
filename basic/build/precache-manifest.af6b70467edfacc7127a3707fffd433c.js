1bc7f8328f3e869ddd8337009e4510688cb0e8b6
imdwpeng <imdwpeng@foxmail.com>
2018-12-28 15:31:55 +0800

    build

---
+++ basic/build/precache-manifest.af6b70467edfacc7127a3707fffd433c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "609860a7b5830bab8c7843e1637290ba",
+    "url": "./build/static/media/bpmn.609860a7.eot"
+  },
+  {
+    "revision": "27b5b1904737f806dcb193a12156af9a",
+    "url": "./build/static/media/bpmn.27b5b190.svg"
+  },
+  {
+    "revision": "c41542e07648518c6c6e",
+    "url": "./build/static/js/runtime~main.c41542e0.js"
+  },
+  {
+    "revision": "bd44791c342d3c00fd22",
+    "url": "./build/static/js/main.bd44791c.chunk.js"
+  },
+  {
+    "revision": "676b2135e7320bfc57b0",
+    "url": "./build/static/js/1.676b2135.chunk.js"
+  },
+  {
+    "revision": "bd44791c342d3c00fd22",
+    "url": "./build/static/css/main.d50aecde.chunk.css"
+  },
+  {
+    "revision": "676b2135e7320bfc57b0",
+    "url": "./build/static/css/1.f248ad69.chunk.css"
+  },
+  {
+    "revision": "e66e3621b1a275fc44f26d921faee41d",
+    "url": "./build/index.html"
+  }
+];
--- basic/build/service-worker.js
+++ basic/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.493eeb14f3724464fd3659d1d55574c4.js"
+  "./build/precache-manifest.af6b70467edfacc7127a3707fffd433c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
