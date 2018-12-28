3803f108682f9588d01e31f9f991c717252a5801
imdwpeng <imdwpeng@foxmail.com>
2018-12-28 15:47:56 +0800

    build

---
+++ basic/build/precache-manifest.da42a894167be24af46383b588d31ed3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "609860a7b5830bab8c7843e1637290ba",
+    "url": "/bpmn-editor/basic/build/static/media/bpmn.609860a7.eot"
+  },
+  {
+    "revision": "27b5b1904737f806dcb193a12156af9a",
+    "url": "/bpmn-editor/basic/build/static/media/bpmn.27b5b190.svg"
+  },
+  {
+    "revision": "638e38a36edd3cec07af",
+    "url": "/bpmn-editor/basic/build/static/js/runtime~main.638e38a3.js"
+  },
+  {
+    "revision": "99538b690043f7f651b8",
+    "url": "/bpmn-editor/basic/build/static/js/main.99538b69.chunk.js"
+  },
+  {
+    "revision": "a9b45286808ec64a6469",
+    "url": "/bpmn-editor/basic/build/static/js/1.a9b45286.chunk.js"
+  },
+  {
+    "revision": "99538b690043f7f651b8",
+    "url": "/bpmn-editor/basic/build/static/css/main.f81b19c5.chunk.css"
+  },
+  {
+    "revision": "a9b45286808ec64a6469",
+    "url": "/bpmn-editor/basic/build/static/css/1.07a7b279.chunk.css"
+  },
+  {
+    "revision": "3bfc8ab78609f07981b59ad6f1b3bccf",
+    "url": "/bpmn-editor/basic/build/index.html"
+  }
+];
