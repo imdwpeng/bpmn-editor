f67009d0826082c1c61732a14b154adf702981b6
imdwpeng <imdwpeng@foxmail.com>
2018-12-28 15:07:23 +0800

    build

---
+++ basic/build/precache-manifest.493eeb14f3724464fd3659d1d55574c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "609860a7b5830bab8c7843e1637290ba",
+    "url": "/static/media/bpmn.609860a7.eot"
+  },
+  {
+    "revision": "27b5b1904737f806dcb193a12156af9a",
+    "url": "/static/media/bpmn.27b5b190.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "020f631c70724e96dc83",
+    "url": "/static/js/main.020f631c.chunk.js"
+  },
+  {
+    "revision": "d7e03294fc2aeca8cce1",
+    "url": "/static/js/1.d7e03294.chunk.js"
+  },
+  {
+    "revision": "020f631c70724e96dc83",
+    "url": "/static/css/main.76b39e7e.chunk.css"
+  },
+  {
+    "revision": "d7e03294fc2aeca8cce1",
+    "url": "/static/css/1.c37bbf21.chunk.css"
+  },
+  {
+    "revision": "16911a253a4b47f22aa4fab4b1d069e3",
+    "url": "/index.html"
+  }
+];
